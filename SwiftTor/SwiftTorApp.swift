f3b98a1827de218ba89fe136bc03f9f5c5671acb
Ruven Schneider <ruven.schneier@gmail.com>
2025-02-09 11:58:18 +0100

    Initial Commit

---
+++ SwiftTor/SwiftTorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTorApp.swift
+//  SwiftTor
+//
+//  Created by Ruven Schneider on 09.02.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
